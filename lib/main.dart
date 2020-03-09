6b86fcbec5b20c9393cb1807655a39570013a902
Sakshi <sakshijinnewar@gmail.com>
2020-03-09 14:07:23 +0530

    transaction  file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expense/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,18 +7,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expense  App',
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List <Transaction> transactions =[
+    Transaction(id:'t1',title:'New Shoes',amount:69.99,date: DateTime.now(),)
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title:   Text('Personal Expense App'),
       ),
       body: Center(
         child: Text('Widget Playground!'),
